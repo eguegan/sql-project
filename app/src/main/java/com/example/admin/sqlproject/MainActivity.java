a81871137dde6c7d6a67f49644c9431ceb521d02
eguegan <e.guegan@margulls.com>
2016-04-11 15:32:56 -0400

    Add startSql method

--- app/src/main/java/com/example/admin/sqlproject/MainActivity.java
+++ app/src/main/java/com/example/admin/sqlproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.sqlproject;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSql(){
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(UsersDatabaseHelper.KEY_USER_NAME, EXAMPLE_NAME);
+            values.put(UsersDatabaseHelper.KEY_AGE, EXAMPLE_AGE);
+
+            db.insertOrThrow(UsersDatabaseHelper.TABLE_USERS, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.e(TAG, "Error while trying to add post to database");
+        } finally {
+            db.endTransaction();
+        }
     }
 }
