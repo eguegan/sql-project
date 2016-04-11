5b3de35c8fcbf1670f30d538dddf6fa5caea59f3
eguegan <e.guegan@margulls.com>
2016-04-11 15:49:32 -0400

    Add method to main activity logInformation()

--- app/src/main/java/com/example/admin/sqlproject/MainActivity.java
+++ app/src/main/java/com/example/admin/sqlproject/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.admin.sqlproject;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String EXAMPLE_NAME = "Edwin";
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void startSql(){
+    public void startSql(View v){
         UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
         SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
 
@@ -34,6 +36,34 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "Error while trying to add post to database");
         } finally {
             db.endTransaction();
+        }
+    }
+
+    public void logInformation(View view) {
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
+
+        final String POSTS_SELECT_QUERY = "SELECT * FROM users";
+
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery(POSTS_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    String nameUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_NAME));
+                    String ageUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_ID));
+
+                    Log.d(TAG, "logInformation: " + nameUser);
+                    Log.d(TAG, "logInformation: " + ageUser);
+
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get posts from database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
         }
     }
 }
